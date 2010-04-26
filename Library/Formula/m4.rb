8904ab04aa340bc63b8159a9e88dc6c93fdb2917
Michael Twentyman <20@kluster.com>
2010-04-26 11:28:53 -0700

    Add m4 library.

---
+++ Library/Formula/m4.rb
@@ -0,0 +1,13 @@
+require 'formula'
+
+class M4 <Formula
+  url 'http://ftp.gnu.org/gnu/m4/m4-1.4.14.tar.gz'
+  homepage ''
+  md5 'f0542d58f94c7d0ce0d01224e447be66'
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "make"
+    system "make install"
+  end
+end
